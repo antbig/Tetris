4fe0b106020844b2de836dca9b67dc560f7c5fcf
antbig <antoine@antbig.fr>
2015-06-01 20:47:20 +0200

    Début du projet

---
+++ Main.c
@@ -0,0 +1,18 @@
+/******************************************************
+**													 **
+**  PROJET TETRIS 2015								 **
+**													 **
+**   Par Leonard Antoine <antoine.leonard@u-psud.fr> **
+**    et David   Pierre  <pierre.david1@u-psud.fr>   **
+**													 **
+**	 tous droits réservés							 **
+** 													 **
+*******************************************************/
+
+
+/**
+  Fonction principale du programme
+**/
+void main(void) {
+	
+}
